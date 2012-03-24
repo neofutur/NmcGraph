6a3f6d3066791df4f0516bb3c0384f9a403a7615
Neo futur <neofutur@ww7.pe>
2012-03-24 11:52:19 +0000

    graph size now a  user choice in the form

--- index.php
+++ index.php
@@ -7,12 +7,15 @@
 <link rel="alternate" type="application/rss+xml" title="Syndicate the whole site" href="http://bitcoin.gw.gd/spip.php?page=backend" />
 <link rel="stylesheet" href="./themes/base/jquery.ui.all.css" />
 <script type="text/javascript" src="./jquery-1.7.1.js"></script>
+<script type="text/javascript" src="./jquery.validate.js"></script>
 <script type="text/javascript" src="./ui/jquery.ui.core.js"></script>
 <script type="text/javascript" src="./ui/jquery.ui.widget.js"></script>
 <script type="text/javascript" src="./ui/jquery.ui.datepicker.js"></script>
+
 <link rel="stylesheet" href="./css/ui-lightness/jquery-ui-1.8.18.custom.css" />
 <link rel="stylesheet" href="./demos.css" />
 <script type="text/javascript" >
+/* <![CDATA[ */
  $(function() {
   $( "#day" ).datepicker({ minDate: new Date(2011, 5, 1), maxDate: "-1D", dateFormat: 'yy-mm-dd', showOtherMonths: true, selectOtherMonths: true,  navigationAsDateFormat: true , autoSize: true, changeMonth: true, changeYear: true });
   });
@@ -22,6 +25,44 @@
  $(function() {
   $( "#year" ).datepicker({ minDate: new Date(2011, 5, 1), maxDate: "-1D", dateFormat: 'yy', showOtherMonths: true, selectOtherMonths: true,  navigationAsDateFormat: true , autoSize: true, changeYear: true });
   });
+ $(document).ready(function() {
+  $( '.xsize').keydown(function(event) {
+        // Allow: backspace, delete, tab and escape
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+  $( '.ysize').keydown(function(event) {
+        // Allow: backspace, delete, tab and escape
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+
+});
+/* ]]> */ 
 </script>
 
 </head>
@@ -34,6 +75,10 @@
 <form action="./daily/nmc_daily.php" method="post">
 <label for="day">choose day</label>
 <input type="text" id="day" name="day" class="datepicker" />
+<br />Optionnaly choose the <label for="xsize">width</label>
+<input type="text" class="xsize" name="xsize" />
+and <label for="ysize">Height</label> for your graph
+<input type="text" class="ysize" name="ysize" />
 <input type="submit" value="go !" />
 </form>
 </div>
@@ -43,6 +88,10 @@
 <form action="./monthly/nmc_monthly.php" method="post">
 <label for="month">Choose month</label>
 <input type="text" id="month" name="month" class="datepicker" />
+<br />Optionnaly choose the <label for="xsize">width</label>
+<input type="text" class="xsize" name="xsize" />
+and <label for="ysize">Height</label> for your graph
+<input type="text" class="ysize" name="ysize" />
 <input type="submit" value="Go !" />
 
 </form>
@@ -54,9 +103,16 @@
 <form action="./yearly/nmc_yearly.php" method="post">
 <label for="day">choose year</label>
 <input type="text" id="year" name="year" class="datepicker" />
+<br />Optionnaly choose the <label for="xsize">width</label>
+<input type="text" class="xsize" name="xsize" />
+and <label for="ysize">Height</label> for your graph
+<input type="text" class="ysize" name="ysize" />
+
 <input type="submit" value="go !" />
+
 </form>
 </div>
+
 
 </body>
 </html>
--- monthly/nmc_monthly.php
+++ monthly/nmc_monthly.php
@@ -7,13 +7,12 @@ include("../../class/pStock.class.php");
 include("../lib/nmcload.php");
 
 if ( isset( $_POST ) && isset( $_POST["month"] ) && is_numeric($_POST["month"] ) ) 
-{
- //received as 03/15/2012 ->2012-03
- $date = $_POST["month"];
- //$date = substr($_POST["month"], 6,4) . "-".substr($_POST["month"],0,2);
- //echo $date;exit;
-}
+{ $date = $_POST["month"]; }
 else $date="2011-06";
+if ( isset( $_POST ) && isset( $_POST["xsize"] ) && is_numeric($_POST["xsize"]) && $_POST["xsize"] <=1024 ) { $xsize = $_POST["xsize"]; }
+else $xsize=800;
+if ( isset( $_POST ) && isset( $_POST["ysize"] ) && is_numeric($_POST["ysize"]) && $_POST["ysize"] <=768 ) { $ysize = $_POST["ysize"]; }
+else $ysize=600;
 
 $minscale=null; $maxscale=null;
 $type="monthly"; $interval="1-day";
@@ -31,22 +30,22 @@ $MyData->setAbscissa("Time");
 //$MyData->setSerieDescription("Time","Hour of the day");
 
 /* Create the pChart object */
-$myPicture = new pImage(800,430,$MyData);
+$myPicture = new pImage($xsize,$ysize,$MyData);
  
 
 $Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>50);
-$myPicture->drawGradientArea(0,0,800,430,DIRECTION_VERTICAL,$Settings);
+$myPicture->drawGradientArea(0,0,$xsize,$ysize,DIRECTION_VERTICAL,$Settings);
  
 /* Draw the border */
-$myPicture->drawRectangle(0,0,799,429,array("R"=>0,"G"=>0,"B"=>0));
+$myPicture->drawRectangle(0,0,$xsize-1,$ysize-1,array("R"=>0,"G"=>0,"B"=>0));
  
 /* Write the title */
 $myPicture->setFontProperties(array("FontName"=>"../../fonts/verdana.ttf","FontSize"=>9));
 $myPicture->drawText(70,45,"NMC price in BTC at date $date",array("FontSize"=>20,"Align"=>TEXT_ALIGN_BOTTOMLEFT));
  
 /* Draw the 1st scale */
-$myPicture->setGraphArea(70,60,760,390);
-$myPicture->drawFilledRectangle(70,60,760,390,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
+$myPicture->setGraphArea(70,60,$xsize-40,$ysize-30);
+$myPicture->drawFilledRectangle(70,60,$xsize-40,$ysize-30,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
 
 $AxisBoundaries = array(0=>array("Min"=>$minscale,"Max"=>$maxscale));
 $myPicture->drawScale(array("DrawSubTicks"=>TRUE,"CycleBackground"=>TRUE,"Mode"=>SCALE_MODE_MANUAL,"ManualScale"=>$AxisBoundaries ));
