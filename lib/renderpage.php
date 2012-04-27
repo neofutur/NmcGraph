de2201de92383d81fce639278b335439a3959bf6
William waisse <neofutur@ww7.pe>
2012-04-27 03:25:26 +0000

    adding header and css tooltips on graph pages too

--- lib/renderpage.php
+++ lib/renderpage.php
@@ -4,7 +4,12 @@ function renderpage($title, $permalink, $altimage, $imagepath, $graphdate, $mess
 {
 $dategen = date ( "Y-m-d G:i:s T Y", $graphdate );
 
-echo "<html><head><title>$title</title></head><body>";
+echo "<html><head><title>$title</title>\n";
+echo "<link rel=\"stylesheet\" href=\"../demos.css\" />\n";
+echo "</head><body>\n";
+
+include("../header.php");
+
 echo "<a href=\"".$permalink."\"> permalink </a> - graph generated on : <b>$dategen</b> - ";
 if ( isset ( $message ) ) echo $message."<br /><br />";
 echo"<img alt=\"".$altimage."\" src=".$imagepath."/>";
