ebe6c5f003bd2a07be69062b0a3cd925fc48b0a5
William waisse <neofutur@ww7.pe>
2012-05-10 22:39:16 +0000

    one more relative path

--- lib/showgraph.php
+++ lib/showgraph.php
@@ -5,7 +5,7 @@ function showgraph( $date, $xsize ="640", $ysize="480Â", $type, $interval )
 {
 include("cfg/nmcgraph_cfg.php");
 $minscale=null; $maxscale=null;
-$imagefilename="../cache/daily/daily_$date.png";
+$imagefilename=dirname(__FILE__). "/../cache/daily/daily_$date.png";
 $imagepath="/cache/daily/daily_$date.png";
 $altimage=$type." namecoin graph for date $date";
 $permalink=$siteurl."/daily/nmc_daily.php?day=".$date;
@@ -13,6 +13,7 @@ $title=$type." namecoin historic data graph for date $date";
 $graphgendate = "";
 $cached=null;
 $message="daily graph is cached one hour";
+//echo $imagefilename; exit;
 if ($graphgendate = checkcache( $imagefilename) )
 {
  $cached=true;
@@ -30,7 +31,7 @@ load_nmc_data($MyData, $date, $type, $minscale, $maxscale);
 $graphgendate = buildgraph( $MyData, $xsize, $ysize, $title,$minscale, $maxscale, $date, $type, $imagefilename  );
 
 }
- $html_code="<img alt=\"".$altimage."\" src=".$imagepath." />";
+ $html_code="<img alt=\"".$altimage."\" src=\"".$imagepath."\" />";
 
  return $html_code;
 }
