288ea922fd78288ddf0bd0a0b3fd4ee6a87e5625
William waisse <neofutur@ww7.pe>
2012-05-10 16:32:03 +0000

    adding header include

---
+++ header.php
@@ -0,0 +1,7 @@
+<div class="perso" style="font-size: 14px; color: black; text-align: center;  width: 100%;align:center;display: block; margin-left: auto; margin-right: auto; ">
+<?php include("banner.php"); ?>
+
+</div>
+<br />
+<div> Reference Prices are from <a href="https://exchange.bitparking.com">bitparking namecoin exchange</a>
+</div>
