19b509cb08c505f4d9175cb65f8797be7034e890
William waisse <neofutur@ww7.pe>
2012-05-10 16:28:22 +0000

    daily now using  checkcache and buildgraph functions

--- daily/nmc_daily.php
+++ daily/nmc_daily.php
@@ -1,12 +1,15 @@
 <?php
 
-include("../../class/pData.class.php");
+/*include("../../class/pData.class.php");
 include("../../class/pDraw.class.php");
 include("../../class/pImage.class.php");
 include("../../class/pStock.class.php");
 include("../lib/nmcload.php");
+*/
 include("../lib/renderpage.php");
+include("../lib/checkcache.php");
 include("../cfg/nmcgraph_cfg.php");
+include("../lib/buildgraph.php");
 
 //var_dump($_POST);exit;
 if ( isset( $_POST ) && isset( $_POST["day"]) )
@@ -33,16 +36,20 @@ $altimage=$type." namecoin graph for date $date";
 $permalink=$siteurl."/daily/nmc_daily.php?day=".$date;
 $title=$type." namecoin historic data graph for date $date";
 $graphgendate = "";
-
+$cached=null;
 $message="daily graph is cached one hour";
+if ($graphgendate = checkcache( $imagefilename) )
+{
+ $cached=true;
 
-$current_time = time(); $expire_time = 3600; $file_time = @filemtime($imagefilename);
-if(file_exists($imagefilename) && ($current_time - $expire_time < $file_time)) {
- //echo 'returning from cached file';
- $graphgendate = $file_time;
+//$current_time = time(); $expire_time = 3600; $file_time = @filemtime($imagefilename);
+//if(file_exists($imagefilename) && ($current_time - $expire_time < $file_time)) {
+// //echo 'returning from cached file';
+// $graphgendate = $file_time;
 }
 else
 {
+ $cached=false;
 /* Create and populate the pData object */
 $MyData = new pData(); 
 load_nmc_data($MyData, $date, $type, $minscale, $maxscale);
@@ -50,73 +57,10 @@ load_nmc_data($MyData, $date, $type, $minscale, $maxscale);
 
 //printf("minscale: $minscale maxscale: $maxscale <br />\n");
 
-//buildgraph( $MyData, $xsize, $ysize, $title  );
-
-$MyData->setAxisDisplay(0,AXIS_FORMAT_CURRENCY, "฿"  );
-//$MyData->addPoints(array("8h","10h","12h","14h","16h","18h"),"Time");
-//$MyData->setAbscissa("Months");
-//$MyData->setAxisName(0,"Price in BTC"); 
-//$MyData->setSerieDescription("Time","Hour of the day");
-
-/* Create the pChart object */
-$myPicture = new pImage($xsize,$ysize,$MyData);
- 
-/* Draw the background */
-$Settings = array("R"=>170, "G"=>183, "B"=>87, "Dash"=>1, "DashR"=>190, "DashG"=>203, "DashB"=>107);
-$myPicture->drawFilledRectangle(0,0,$xsize,$ysize,$Settings);
- 
-/* Overlay with a gradient */
-$Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>50);
-$myPicture->drawGradientArea(0,0,$xsize,$ysize,DIRECTION_VERTICAL,$Settings);
- 
-/* Draw the border */
-$myPicture->drawRectangle(0,0,$xsize-1,$ysize-1,array("R"=>0,"G"=>0,"B"=>0));
- 
-/* Write the title */
-$myPicture->setFontProperties(array("FontName"=>"../../fonts/verdana.ttf","FontSize"=>9));
-$myPicture->drawText(70,45,$title,array("FontSize"=>20,"Align"=>TEXT_ALIGN_BOTTOMLEFT));
- 
-/* Draw the 1st scale */
-$myPicture->setGraphArea(70,60,$xsize-40,$ysize-30);
-$myPicture->drawFilledRectangle(70,60,$xsize-40,$ysize-30,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
-
-// Y scale
-$AxisBoundariesY = array(0=>array("Min"=>$minscale,"Max"=>$maxscale));
-$myPicture->drawScale(array("DrawSubTicks"=>TRUE,"CycleBackground"=>TRUE,"Mode"=>SCALE_MODE_MANUAL,"ManualScale"=>$AxisBoundariesY ));
-
-//X scale
-//$AxisBoundariesX = array("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23");
-//$MyData->setSerieDescription("Labels","Months");
-//$MyData->setAbscissa("Labels");
-$MyData->setXAxisDisplay(0,AXIS_FORMAT_TIME,"H");
-//$myPicture->drawScale(array("DrawXLines"=>array(0)));
- 
-/* Draw the 1st stock chart */
-$mystockChart = new pStock($myPicture,$MyData);
-$myPicture->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>30));
-$mystockChart->drawStockChart();
- 
-/* Reset the display mode because of the graph small size */
-//$MyData->setAxisDisplay(0,AXIS_FORMAT_DEFAULT);
- 
-/* Draw the 2nd scale */
-//$myPicture->setShadow(FALSE);
-//$myPicture->setGraphArea(500,60,670,190);
-//$myPicture->drawFilledRectangle(500,60,670,190,array("R"=>255,"G"=>255,"B"=>255,"Surrounding"=>-200,"Alpha"=>10));
-//$myPicture->drawScale(array("Pos"=>SCALE_POS_TOPBOTTOM,"DrawSubTicks"=>TRUE));
- 
-/* Draw the 2nd stock chart */
-//$mystockChart = new pStock($myPicture,$MyData);
-//$myPicture->setShadow(TRUE,array("X"=>1,"Y"=>1,"R"=>0,"G"=>0,"B"=>0,"Alpha"=>30));
-//$mystockChart->drawStockChart();
- 
-/* Render the picture (choose the best way) */
-//$myPicture->autoOutput("pictures/example.drawStockChart.png");
-$myPicture->render($imagefilename);
-$graphgendate = $current_time;
+$graphgendate = buildgraph( $MyData, $xsize, $ysize, $title,$minscale, $maxscale  );
 
 }
-renderpage($title, $permalink, $altimage, $imagepath, $graphgendate, $message );
+renderpage($title, $permalink, $altimage, $imagepath, $graphgendate, $message,$cached );
 
 /*echo "<html><head></head><body>";
 echo "<a href=\"".$permalink."\"> permalink </a><br /><br />";
--- lib/renderpage.php
+++ lib/renderpage.php
@@ -1,6 +1,6 @@
 <?php
 
-function renderpage($title, $permalink, $altimage, $imagepath, $graphdate, $message="" )
+function renderpage($title, $permalink, $altimage, $imagepath, $graphdate, $message="", $cached=false )
 {
 $dategen = date ( "Y-m-d G:i:s T Y", $graphdate );
 
@@ -9,9 +9,12 @@ echo "<link rel=\"stylesheet\" href=\"../demos.css\" />\n";
 echo "</head><body>\n";
 
 include("../header.php");
-
+if ( $cached == true )
+ $cache_message="cached graph";
+else
+ $cache_message="new graph generated now";
 echo "<a href=\"".$permalink."\"> permalink </a> - graph generated on : <b>$dategen</b> - ";
-if ( isset ( $message ) ) echo $message."<br /><br /><br />";
+if ( isset ( $message ) ) echo $message." -  $cache_message "."<br /><br /><br />";
 echo"<img alt=\"".$altimage."\" src=".$imagepath."/>";
 include( "../footer.php");
 echo "</body></html>";
